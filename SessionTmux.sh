de9123020e731723a208128f036837ae0d33516d
0xZxcrypto <74732301+0xZxcrypto@users.noreply.github.com>
2025-02-01 01:02:05 +0700

    Update SessionTmux.sh

--- SessionTmux.sh
+++ SessionTmux.sh
@@ -24,8 +24,8 @@ spinner() {
 # Header dan pesan pembuka dengan gambar ASCII
 clear
 echo -e "${GREEN}==========================================================="
-echo -e "${YELLOW}            ▀█ ▀▄▀ █▀▀ █▀█ █▄█ █▀█ ▀█▀ █▀█
-echo -e "${YELLOW}            █▄ █░█ █▄▄ █▀▄ ░█░ █▀▀ ░█░ █▄█                                         
+echo -e "${YELLOW}            ▀█ ▀▄▀ █▀▀ █▀█ █▄█ █▀█ ▀█▀ █▀█            "
+echo -e "${YELLOW}            █▄ █░█ █▄▄ █▀▄ ░█░ █▀▀ ░█░ █▄█            "
 echo -e "${GREEN}==========================================================="
 echo -e "${YELLOW}       ⚡ WELCOME TO THE AUTOMATED BOT LAUNCHER ⚡               "
 echo -e "${GREEN}==========================================================="
